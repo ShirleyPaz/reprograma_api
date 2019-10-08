75872f013d7200ffe62e1774b18a45a6418d19a4
shirleypaz <shirley.pazz@gmail.com>
2019-10-08 20:02:33 -0300

    first commit

---
+++ src/controllers/alunasController.js
@@ -0,0 +1,12 @@
+const alunas = require('../models/alunas.json');
+
+exports.get = (req, res) => {
+    console.log(req.url);
+    res.status(200).send(alunas)
+}
+
+exports.getById = (req, res) => {
+    const id = req.params.id
+    console.log(id);
+    res.status(200).send(alunas.find(aluna => aluna.id == id))
+}
