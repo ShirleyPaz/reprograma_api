40530d52db7db98edf9cd24bb9f94a7f29acca40
shirleypaz <shirley.pazz@gmail.com>
2019-10-08 22:21:09 -0300

    criação rota de livros lidos por aluna

--- src/controllers/alunasController.js
+++ src/controllers/alunasController.js
@@ -10,3 +10,21 @@ exports.getById = (req, res) => {
     console.log(id);
     res.status(200).send(alunas.find(aluna => aluna.id == id))
 }
+
+exports.getBooks = (req, res) => {
+    const id = req.params.id
+    // res.status(200).send(alunas.find(aluna => aluna.id == id).livros.reduce((livrosLidos, livro) => {
+    //     if (livro.leu === "true") {
+    //         livrosLidos.push(livro.titulo);
+    //     }
+    //     return livrosLidos;
+    // }, [])
+    const livrosLidos = []
+    alunas.find(aluna => aluna.id == id).livros.map(livro => {
+        if (livro.leu === "true") {
+            livrosLidos.push(livro.titulo);
+        }
+    })
+    res.status(200).send(livrosLidos);
+}
+
--- src/routes/alunasRoute.js
+++ src/routes/alunasRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const alunasController = require('../controllers/alunasController')
+const alunasController = require('../controllers/alunasController');
 
 router.get('/', alunasController.get);
-router.get('/:id', alunasController.getById)
+router.get('/:id', alunasController.getById);
+router.get('/:id/livros', alunasController.getBooks);
 
 module.exports = router;
