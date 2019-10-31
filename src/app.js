13fa2d90dae1f2f7b0c1945d40cb0c572786668e
shirleypaz <shirley.pazz@gmail.com>
2019-10-30 22:16:01 -0300

    adicionado express json

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const app = express();
 
-
 // rotas
 const index = require('./routes/index');
 const alunas = require('./routes/alunasRoute');
+const professoras = require('./routes/professorasRoute');
 
-app.all('*', function(req, res, next) {
-    console.log('passando pelo app')
-    next()
-});
+app.use(express.json());
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-Width, Content-Type, Accept')
+    next();
+})
 app.use('/', index);
 app.use('/alunas', alunas);
+app.use('/professoras', professoras);
 
 module.exports = app;
