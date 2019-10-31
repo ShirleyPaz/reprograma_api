9e449ad6628b41d0e491d0febaff0f0efc82b19a
shirleypaz <shirley.pazz@gmail.com>
2019-10-30 22:17:08 -0300

    criação post alunas e livro por aluna

--- src/controllers/alunasController.js
+++ src/controllers/alunasController.js
@@ -1,8 +1,21 @@
 const alunas = require('../models/alunas.json');
+const fs = require('fs');
 
 exports.get = (req, res) => {
     console.log(req.url);
     res.status(200).send(alunas)
+}
+
+exports.post = (req, res) => {
+    const { nome, dateOfBirth, nasceuEmSp, id, livros } = req.body;
+    alunas.push({ nome, dateOfBirth, nasceuEmSp, id, livros });
+    fs.writeFile("./src/models/alunas.json", JSON.stringify(alunas), 'utf-8', function(err) {
+        if(err) {
+            return res.status(500).send({message: err});
+        }
+    });
+
+    res.status(201).send(alunas);
 }
 
 exports.getById = (req, res) => {
@@ -19,7 +32,7 @@ exports.getBooks = (req, res) => {
     const aluna = alunas.find(aluna => aluna.id == id);
 
     if (!aluna) {
-        res.send('Não encontrei essa garota');
+       return res.send('Não encontrei essa garota');
     }
     // res.status(200).send(alunas.find(aluna => aluna.id == id).livros.reduce((livrosLidos, livro) => {
     //     if (livro.leu === "true") {
@@ -36,12 +49,71 @@ exports.getBooks = (req, res) => {
     res.status(200).send(livrosLidos);
 }
 
+exports.postBooks = (req, res) => {
+    const id = req.params.id;
+    const aluna = alunas.find(aluna => aluna.id == id);
+
+    if (!aluna) {
+        return res.send('Não encontrei essa garota');
+    }
+  
+    // salvar o novo livro
+    const { titulo, leu } =  req.body;
+    alunas[id - 1].livros.push({ titulo, leu });
+
+    fs.writeFile("./src/models/alunas.json", JSON.stringify(alunas), 'utf-8', function(err) {
+        if(err) {
+            return res.status(500).send({ message: err })
+        }
+        console.log('livro salvo com sucesso')
+    })
+
+    res.status(200).send(alunas);
+}
+
 exports.getAlunasSp = (req, res) => {
     console.log('ENTROU NO ALUNAS SP')
-    const listaNasceuSP = alunas.filter( aluna => aluna.nasceuEmSp == "true" ).reduce((listaNomes, item) => {
+    const listaNasceuSP = alunas.filter(aluna => aluna.nasceuEmSp == "true").reduce((listaNomes, item) => {
         listaNomes.push(item.nome);
         return listaNomes;
     }, [])
     res.status(200).send(listaNasceuSP);
+}
+
+exports.getIdade = (req, res) => {
+    const id = req.params.id;
+    const aluna = alunas.find(e => e.id == id);
+    const dataNascimento = new Date(aluna.dateOfBirth);
+
+    const anoDeNasc = aluna.dateOfBirth.split('/')[2];
+    const mesDeNasc = aluna.dateOfBirth.split('/')[1];
+    const diaDeNasc = aluna.dateOfBirth.split('/')[0];
+
+    // const anoDeNasc = dataNascimento.getFullYear()
+    // const mesDeNasc = dataNascimento.getMonth() + 1
+    // const diaDeNasc = dataNascimento.getDate()
+
+
+    console.log(id, aluna, anoDeNasc, mesDeNasc, diaDeNasc)
+
+
+    function calcularIdade(anoDeNasc, mesDeNasc, diaDeNasc) {
+        const now = new Date()
+        const anoAtual = now.getFullYear()
+        const mesAtual = now.getMonth() + 1
+        const hoje = now.getDate()
+
+        let idade = anoAtual - anoDeNasc
+
+        if (mesAtual < mesDeNasc || (mesAtual == mesDeNasc && hoje < diaDeNasc)) {
+            idade -= 1
+        }
+        return idade
+    }
+
+    const idade = calcularIdade(anoDeNasc, mesDeNasc, diaDeNasc)
+    console.log(typeof idade, 'idade');
+
+    res.status(200).send(`A idade da aluna é ${idade}`);
 }
 
--- src/routes/alunasRoute.js
+++ src/routes/alunasRoute.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const alunasController = require('../controllers/alunasController');
+const fs = require('fs');
 
 router.get('/', alunasController.get);
 router.get('/nasceuSp', alunasController.getAlunasSp);
 router.get('/:id', alunasController.getById);
 router.get('/:id/livros', alunasController.getBooks);
+router.get('/:id/idade', alunasController.getIdade);
+router.post('/', alunasController.post);
+router.post('/:id/livros', alunasController.postBooks);
 
 module.exports = router;
